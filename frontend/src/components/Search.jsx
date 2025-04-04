4a8e5882f9b785bc0568f098a2d5768bc4fba1b9
rukunuddin29 <rukunuddin2003@gmail.com>
2025-04-04 19:10:16 +0530

    08

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -4,7 +4,8 @@ import { FaSearch } from "react-icons/fa";
 function Search() {
   return (
     <div className="w-full flex items-center justify-center px-4 py-6">
-      <div className="w-full max-w-4xl border border-gray-200 rounded-full flex flex-col sm:flex-row items-center bg-white shadow-lg p-2 gap-2 sm:gap-4">
+<div className="w-full max-w-4xl border border-gray-200 rounded-2xl lg:rounded-full flex flex-col sm:flex-row items-center bg-white shadow-lg p-2 gap-2 sm:gap-4">
+
         
         {/* Destination */}
         <div className="flex-1 w-full sm:w-auto px-3 py-2 rounded-full hover:bg-gray-100 transition-colors duration-200">
@@ -35,7 +36,7 @@ function Search() {
         </div>
 
         {/* Number of Guests */}
-        <div className="flex-1 w-full sm:w-auto px-3 py-2 rounded-full hover:bg-gray-100 transition-colors duration-200">
+        <div className="flex-1 w-full sm:w-auto px-3 py-2 rounded-full  hover:bg-gray-100 transition-colors duration-200">
           <h4 className="text-xs font-semibold text-gray-600 uppercase tracking-wide">Guests</h4>
           <input
             type="number"
